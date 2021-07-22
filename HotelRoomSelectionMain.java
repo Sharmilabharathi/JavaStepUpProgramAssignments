c253f647efa3dea1c25bc6d2aab5e175777ff99a
Sharmila <sharmilapriya94@gmail.com>
2021-07-22 16:07:23 +0530

    JavaStepUpTraining Activity3 change

--- HotelRoomSelectionMain.java
+++ HotelRoomSelectionMain.java
@@ -9,22 +9,31 @@ public class HotelRoomSelectionMain {
                 "2. Deluxe AC Room\n" +
                 "3. Suite AC Room\n");
         System.out.println("Select Room Type:");
-        Integer roomType = new Scanner(System.in).nextInt();
+        int roomType = new Scanner(System.in).nextInt();
         System.out.println("Hotel Name:");
         String hotelName = new Scanner(System.in).nextLine();
         System.out.println("Room Square Feet Area:");
-        Integer numberOfSqFeet = new Scanner(System.in).nextInt();
+        int numberOfSqFeet = new Scanner(System.in).nextInt();
         System.out.println("Room has TV (yes/no):");
         String tvSelection = new Scanner(System.in).nextLine();
         System.out.println("Room has Wifi (yes/no):");
         String wifiSelection = new Scanner(System.in).nextLine();
 
-        Boolean hasTV = getTvSelection(tvSelection);
-        Boolean hasWifi = getWifiSelection(wifiSelection);
+        boolean hasTV = getTvSelection(tvSelection);
+        boolean hasWifi = getWifiSelection(wifiSelection);
 
-        DeluxeRoom deluxeRoom = new DeluxeRoom(hotelName,numberOfSqFeet,hasTV,hasWifi);
-        DeluxeSeaViewRoom deluxeACRoom = new DeluxeSeaViewRoom(hotelName,numberOfSqFeet,hasTV,hasWifi);
-        SuiteRoom suiteRoom = new SuiteRoom(hotelName,numberOfSqFeet,hasTV,hasWifi);
+        DeluxeRoom deluxeRoom = new DeluxeRoom(hotelName,
+                numberOfSqFeet,
+                hasTV,
+                hasWifi);
+        DeluxeSeaViewRoom deluxeACRoom = new DeluxeSeaViewRoom(hotelName,
+                numberOfSqFeet,
+                hasTV,
+                hasWifi);
+        SuiteRoom suiteRoom = new SuiteRoom(hotelName,
+                numberOfSqFeet,
+                hasTV,
+                hasWifi);
         Integer costPerSquareFeet,calculateHotelTariff;
 
         if(roomType==1){
@@ -47,20 +56,18 @@ public class HotelRoomSelectionMain {
     }
 
     public static Boolean getTvSelection(String hasTV){
-        if(hasTV.equalsIgnoreCase("yes")){
-             return true;
-        }else{
+        if(hasTV.equalsIgnoreCase("yes"))
+            return true;
+        else
             return false;
-        }
 
     }
 
     public static Boolean getWifiSelection(String hasWifi){
-        if(hasWifi.equalsIgnoreCase("yes")){
+        if(hasWifi.equalsIgnoreCase("yes"))
             return true;
-        }else{
+        else
             return false;
-        }
 
     }
 }
